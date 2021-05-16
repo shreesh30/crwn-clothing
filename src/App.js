a96729a2811d0eb14e9002c25b304669705ce673
shreesh <shreesharya20@gmail.com>
2021-05-16 19:06:00 +0530

    created collection-preview

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import {Switch,Route} from 'react-router-dom';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
-
-const HatsPage=()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' render={HomePage}/>
-        <Route path='/hats' render={HatsPage}/>
-
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
