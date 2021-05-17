efdc9fc987d3b65488c1ddc2868ca57d9b67ab2e
shreesh <shreesharya20@gmail.com>
2021-05-17 18:41:26 +0530

    added a sign in form and created custom input and button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
