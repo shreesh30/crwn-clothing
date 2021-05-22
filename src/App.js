b0857a6af6818f3ef686641237be2d2751f0eb3d
shreesh <shreesharya20@gmail.com>
2021-05-22 19:35:33 +0530

    added sign up component with email,name and password

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import { auth ,createUserProfileDocument} from "../src/firebase/firebase.utils"
-
+import {
+  auth,
+  createUserProfileDocument,
+} from "../src/firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -15,15 +17,29 @@ class App extends React.Component {
       currentUser: null,
     };
   }
-  unsubscribeFromAuth=null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-   this.unsubscribeFromAuth= auth.onAuthStateChanged(async (user) => {
-    createUserProfileDocument(user) 
-    // this.setState({ currentUser: user });
-      // console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
+
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -31,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,21 +18,22 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
+
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log(`error creating user`,error.message)
+      console.log("error creating user", error.message);
     }
   }
+
   return userRef;
 };
 firebase.initializeApp(config);
