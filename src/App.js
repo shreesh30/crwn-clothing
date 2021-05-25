e6f49ad83d3a2de5b9065299abfe447f4a54b020
shreesh <shreesharya20@gmail.com>
2021-05-25 19:27:44 +0530

    added userReducer and root-reducer

--- src/App.js
+++ src/App.js
@@ -20,24 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
               id: snapShot.id,
-              ...snapShot.data()
-            }
+              ...snapShot.data(),
+            },
           });
         });
+      } else {
+        this.setState({ currentUser: userAuth });
       }
-
-      this.setState({ currentUser: userAuth });
     });
   }
-
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
