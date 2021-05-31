312b9c3ecac6df5e180f259869976f71c19e090e
shreesh <shreesharya20@gmail.com>
2021-05-31 19:06:00 +0530

    changed directories and shop to reducers of their own

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import { createStructuredSelector } from "reselect";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({id,...otherCollectionProps})=>(
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
