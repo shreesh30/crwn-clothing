9bf656a40e3e8eaf6be7abc91597286f0712a69d
shreesh <shreesharya20@gmail.com>
2021-06-07 19:59:25 +0530

    finished collection-preview component and moved our directory into its own reducer,created respective selectors and updated corresponding component with redux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,10 +5,15 @@ import { collect, connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h1>Collection page</h1>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
