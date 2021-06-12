dc89abd0d0e1e18fa202165a047dafc0ee9fe0c1
shreesh <shreesharya20@gmail.com>
2021-06-12 15:09:50 +0530

    changes from scss to styled component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import { addItem } from "../../redux/cart/cart.actions";
 import {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import "./sign-in.styles.scss";
-
 import FormInput from "../form-input/form-input.component";
 
 import CustomButton from "../../components/custom-button/custom-button.component";
 
 import { auth, SignInWithGoogle } from "../../firebase/firebase.utils.js";
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer,
+} from "./sign-in.styles";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -27,7 +30,7 @@ class SignIn extends React.Component {
         password: "",
       });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
     this.setState({ email: "", password: "" });
   };
@@ -38,8 +41,8 @@ class SignIn extends React.Component {
   };
   render() {
     return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
           <FormInput
@@ -58,14 +61,14 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <div className="buttons">
+          <ButtonsBarContainer>
             <CustomButton type="submit">Sign In</CustomButton>
             <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
               Sign In with Google
             </CustomButton>
-          </div>
+          </ButtonsBarContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
